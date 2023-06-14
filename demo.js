992d788985a989cf47d32ffedbd30083b653e743
vikaskumar2500 <vikaskumar808425@gmail.com>
2023-06-14 08:54:21 +0530

    fourth commit

--- demo.js
+++ demo.js
@@ -1,1 +1,11 @@
-console.log("demo of DOM Class");
+let items = document.getElementsByClassName("list-group-item");
+// console.log(items);
+
+items[2].style.backgroundColor = "green";
+
+// so here getElementsByClassName gives the htmlCollection 
+// in htmlCollection we don't use the forEach loop only exccesing by the indexes
+
+for(let i=0;i<items.length;i++) {
+    items[i].style.fontWeight = "bold";
+}
