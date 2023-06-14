cc168a0af37c2b6f5947bece53b6a4975d45feec
vikaskumar2500 <vikaskumar808425@gmail.com>
2023-06-14 15:20:40 +0530

    querySelector commit

---
+++ querySelector.js
@@ -0,0 +1,9 @@
+
+//querySelector
+const secondItem = document.querySelector("li:nth-child(2)");
+// console.log(secondItem);
+secondItem.style.backgroundColor = "green";
+
+const thirdItem = document.querySelector("li:nth-child(3)");
+console.log(thirdItem)
+thirdItem.style.display="none";
