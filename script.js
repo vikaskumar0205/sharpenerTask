203af1058a9ccf99412356e60cbb285991326865
vikaskumar2500 <vikaskumar808425@gmail.com>
2023-06-19 16:11:15 +0530

    update async await

--- script.js
+++ script.js
@@ -1,86 +1,56 @@
-// const getColdDrinks = async ()=> {
-//     const promise = await new Promise((resolve, reject)=>{
-//         setTimeout(()=>{
-//             const getButter = true;
+const getColdDrinks = async ()=> {
+    const promise = await new Promise((resolve, reject)=>{
+        setTimeout(()=>{
+            const getButter = true;
 
-//             if(getButter) {
-//                 resolve("now let's got for the cold drinks");
-//             }
-//             else {
-//                 reject("sorry , darling!! ")
-//             }
-//         }, 1000);
-//     })
-//     console.log(promise);
-// }
-// getColdDrinks();
-
-// 2nd part of this question
-
-// async function updateLastUserActivityTime() {
-//     return await new Promise((resolve, reject)=>{
-//         setTimeout(()=>{
-//             const date = new Date();
-//             resolve();
-//         },1000);
-//     });
-// };
-// async function createPost() {
-//     return await new Promise((resolve, reject)=>{
-//         setTimeout(()=>{
-//             posts.push({title:"POST"});
-//             resolve();
-//         }, 1000);
-//     });
-// };
-// async function deletePost() {
-//     return await new Promise((resolve, reject)=>{
-//         let n = posts.length;
-//         if(n>0) {
-//             const popEle = posts[n-1];
-//             posts.pop();
-//             resolve(popEle);
-//         }
-//         else reject("ERROR");
-//     })
-// }
-
-async function update() {
-  const posts = [];
-  const updateLastUserActivityTime = new Promise((resolve, reject) => {
-    setTimeout(() => {
-      const date = new Date();
-      resolve();
-    }, 1000);
-  });
-
-  
-  const createPost = new Promise((resolve, reject) => {
-    setTimeout(() => {
-      posts.push({ title: "POST" });
-      resolve();
-    }, 1000);
-  });
-
-  let [udateUseTime, post] = await Promise.all([
-    updateLastUserActivityTime,
-    createPost,
-  ]);
-  console.log(`${udateUseTime} and ${post}`);
+            if(getButter) {
+                resolve("now let's got for the cold drinks");
+            }
+            else {
+                reject("sorry , darling!! ")
+            }
+        }, 1000);
+    })
+    console.log(promise);
 }
-update();
-// const deletePost = new Promise((resolve, reject) => {
-//   let n = posts.length;
-//   if (n > 0) {
-//     const popEle = posts[n - 1];
-//     posts.pop();
-//     resolve(popEle);
-//   } else reject("ERROR");
-// });
-// try{
-// Promise.all([updateLastUserActivityTime(), createPost()])
-//     .then(deletePost)
-//     .then(data=>console.log(data.title));
-// }catch(error){
-//     console.log(error)
-// };
+getColdDrinks();
+
+
+// create and delete post using async and await
+try {
+  const posts = [{ title: "POST" }];
+  let i=1;
+  async function createPost() {
+    return await new Promise((resolve) => {
+      setTimeout(() => {
+        posts.push({ title: `POST${i++}` });
+
+        resolve(posts);
+      }, 1000);
+    });
+  }
+  async function deletePost(){
+      return await new Promise((resolve, reject)=>{
+
+          setTimeout(()=>{
+              // reject("Emptys posts");
+
+              if(posts.length > 0 ) {
+                const poppedEle = posts.pop();
+                resolve(poppedEle);
+              }
+              else reject("Emptys posts");
+          }, 5000)
+
+      })
+  }
+  
+  let data =  Promise.all([createPost(createPost(createPost())),deletePost(), deletePost()]);
+//   console.log(data)
+  data.then(item=>{
+    item.forEach(ele=>console.log(ele));
+  }).catch(error=>console.log(error));
+  
+} catch (error) {
+  console.log(error);
+}
