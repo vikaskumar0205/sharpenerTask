622378e3b303d83cb44596ac1c7ac07da0074623
vikaskumar0205 <vikaskumar808425@gmail.com>
2023-07-02 10:33:00 +0530

    updated promise.all

--- script.js
+++ script.js
@@ -1,40 +1,39 @@
-
 const posts = [];
 
 function updateLastUserActivityTime() {
-    return new Promise((resolve, reject)=>{
-        setTimeout(()=>{
-            const date = new Date();
-            resolve();
-        },1000);
-    });
-};
-
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      const date = new Date();
+      resolve();
+    }, 1000);
+  });
+}
 
 function createPost() {
-    return new Promise((resolve, reject)=>{
-        setTimeout(()=>{
-            posts.push({title:"POST"});
-            resolve();
-        }, 1000);
-    });
-};
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      posts.push({ title: "POST" });
+      resolve();
+    }, 1000);
+  });
+}
 
 function deletePost() {
-    return new Promise((resolve, reject)=>{
-        let n = posts.length;
-        if(n>0) {
-            const popEle = posts[n-1];
-            posts.pop();
-            resolve(popEle);
-        }
-        else reject("ERROR");
-    })
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      if (posts.length > 0) {
+        const popEle = posts.pop();
+        resolve(popEle);
+      } else reject("ERROR");
+    }, 1000);
+  });
 }
-try{
-Promise.all([updateLastUserActivityTime(), createPost()])
+try {
+  Promise.all([updateLastUserActivityTime(), createPost(), createPost()])
+    .then(deletePost)
+    .then((data) => console.log(data.title))
     .then(deletePost)
     .then(data=>console.log(data.title));
-}catch(error){
-    console.log(error)
-};
+} catch (error) {
+  console.log(error);
+}
